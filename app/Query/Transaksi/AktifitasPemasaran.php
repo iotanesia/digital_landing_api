498d5d6f13acd60f6a464770351ef8d3330c1a8c
muhamadlutfikamil <kamillutfi505@gmail.com>
2022-07-09 08:52:23 +0700

    Update AktifitasPemasaran.php

--- app/Query/Transaksi/AktifitasPemasaran.php
+++ app/Query/Transaksi/AktifitasPemasaran.php
@@ -165,7 +165,8 @@ class AktifitasPemasaran {
                 $params['is_cutoff'] = 0;
             }
 
-            $image = $request->foto;  // your base64 encoded
+            $imageKtp = $request->foto_ktp;
+            $imageSelfie = $request->foto_selfie;  // your base64 encoded
             $params['foto_ktp'] = (string) Str::uuid().'.png';
             $params['foto_selfie'] = (string) Str::uuid().'.png';
             $params['nomor_aplikasi'] = Helper::generateNoApliksi($request->current_user->id_cabang);
@@ -179,8 +180,8 @@ class AktifitasPemasaran {
                 ]));
             }
             // after commit process
-            Storage::put($params['foto_ktp'], base64_decode($image));
-            Storage::put($params['foto_selfie'], base64_decode($image));
+            Storage::put($params['foto_ktp'], base64_decode($imageKtp));
+            Storage::put($params['foto_selfie'], base64_decode($imageSelfie));
             $mail_data = [
                 "fullname" => $store->nama,
                 "nik" => $store->nik,
@@ -217,7 +218,8 @@ class AktifitasPemasaran {
             $update = Model::find($id);
             if(!$update) throw new \Exception("Data not found.", 400);
             $params = $request->all();
-            $params['id_user'] = request()->current_user->id;
+            $params['id_user'] = $request->current_user->id;
+            $params['id_cabang'] = $request->current_user->id_cabang;
 
             if ((int) $request->status === 2) {
                 $cekPipeline = ModelPipeline::where('nik', $request->nik)->first();
@@ -247,12 +249,23 @@ class AktifitasPemasaran {
                 $params['is_cutoff'] = 0;
             }
 
-            $image = $request->foto;  // your base64 encoded
-            $params['foto'] = (string) Str::uuid().'.png';
+            if ($request->foto_ktp) {
+                $imageKtp = $request->foto_ktp;  // your base64 encoded
+                $params['foto_ktp'] = (string) Str::uuid().'.png';
+                Storage::put($params['foto_ktp'], base64_decode($imageKtp));
+                $params['foto_ktp'] = (string) Str::uuid().'.png';
+            }
+
+            if ($request->foto_selfie) {
+                $imageselfie = $request->foto_selfie;  // your base64 encoded
+                $params['foto_selfie'] = (string) Str::uuid().'.png';
+                Storage::put($params['foto_selfie'], base64_decode($imageselfie));
+                $params['foto_selfie'] = (string) Str::uuid().'.png';
+            }
             $update->update($params);
             if($is_transaction) DB::commit();
             // after commit process
-            Storage::put($params['foto'], base64_decode($image));
+            
             $mail_data = [
                 "fullname" => $update->nama,
                 "nik" => $update->nik,
@@ -269,7 +282,6 @@ class AktifitasPemasaran {
                 $reqRiwayat['id_tujuan_pemasaran'] = $request->id_tujuan_pemasaran;
                 $reqRiwayat['id_cara_pemasaran'] = $request->id_cara_pemasaran;
                 $reqRiwayat['informasi_aktifitas'] =  $request->informasi_aktifitas;
-                $reqRiwayat['foto'] = $params['foto'];
                 $reqRiwayat['lokasi'] = $request->lokasi;
                 $store = ModelRiwayat::create($reqRiwayat);
             }
