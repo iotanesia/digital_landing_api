c81c0afb14274903daf8e53815d4b33619dcf413
rexy siwabessy <rexysiwabessy@gmail.com>
2022-07-15 22:15:31 +0700

    update approval ps

--- app/Query/Transaksi/ApprovalPrescreening.php
+++ app/Query/Transaksi/ApprovalPrescreening.php
@@ -1,16 +1,45 @@
 <?php
 
 namespace App\Query\Transaksi;
+
+use App\ApiHelper AS Helper;
 use App\Query\Transaksi\AktifitasPemasaran;
 use App\Query\Transaksi\Leads;
 use App\Query\Transaksi\Eform;
+use App\View\Transaksi\VPrescreening AS View;
 use Illuminate\Support\Facades\DB;
 
 class ApprovalPrescreening {
 
     public static function getDataCurrent($request)
     {
-        # code...
+        try {
+            $data = View::where(function ($query) use ($request){
+                $query->where('is_prescreening',2);
+                if($request->nik) $query->where('nik',$request->nik);
+                if($request->tipe_calon_nasabah) $query->where('tipe_calon_nasabah',$request->tipe_calon_nasabah);
+            })->paginate($request->limit);
+            return [
+                'items' => $data->getCollection()->transform(function ($item){
+                    return [
+                        'id' => $item->id,
+                        'nik' => $item->nik,
+                        'nama' => $item->nama,
+                        'tipe_calon_nasabah' => $item->tipe_calon_nasabah,
+                        'foto_selfie' => $item->foto_selfie,
+                        'status_prescreening'=> 'Lolos Bersyarat'
+                    ];
+                }),
+                'attributes' => [
+                    'total' => $data->total(),
+                    'current_page' => $data->currentPage(),
+                    'from' => $data->currentPage(),
+                    'per_page' => (int) $data->perPage(),
+                ]
+            ];
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     public static function byId($id)
