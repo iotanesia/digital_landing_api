b114ba53ff184603984b92ff6351a5dc05a4103d
aleekhabib <aleekhabib@gmail.com>
2022-07-16 12:28:36 +0700

    fix : cabang

--- app/Query/Master/MCabang.php
+++ app/Query/Master/MCabang.php
@@ -30,7 +30,7 @@ class MCabang {
             if($request->dropdown == Constants::IS_ACTIVE) $request->limit = Model::count();
             $data = Model::where(function ($query) use ($request,$check_propinsi,$check_kabupaten,$check_kecamatan){
                 if($request->nama_cabang) $query->where('nama_cabang','ilike',"%$request->nama_cabang%");
-                if($check_propinsi) $query->where('id_propinsi',$request->id_propinsi);
+                $query->where('id_propinsi',$request->id_propinsi);
                 if($check_kabupaten) $query->where('id_kabupaten',$request->id_kabupaten);
                 if($check_kecamatan) $query->where('id_kecamatan',$request->id_kecamatan);
                 $query->whereNotNull('lat');
