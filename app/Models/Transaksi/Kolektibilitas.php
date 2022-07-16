7b7b351365ed4f7211d7dda5b0b3832a8020bccd
aleekhabib <aleekhabib@gmail.com>
2022-07-16 12:23:05 +0700

    fix : informasi prescreening

--- app/Models/Transaksi/Kolektibilitas.php
+++ app/Models/Transaksi/Kolektibilitas.php
@@ -11,4 +11,15 @@ class Kolektibilitas extends Model
     use HasFactory, SoftDeletes;
     protected $table = 'kolektibilitas';
     protected $connection = 'transaksi';
+
+    protected $hidden = [
+        'deleted_at',
+        'updated_at',
+        'updated_by',
+        'created_at',
+        'created_by',
+        'nomor_aplikasi',
+        'nik',
+        'id',
+    ];
 }
--- app/View/Transaksi/VListPipeline.php
+++ app/View/Transaksi/VListPipeline.php
@@ -2,16 +2,20 @@
 
 namespace App\View\Transaksi;
 
+use App\Models\Transaksi\Kolektibilitas;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class VListPipeline extends Model {
-    use HasFactory;
 
+    use HasFactory;
     protected $table = 'v_list_pipeline';
     protected $connection = 'transaksi';
 
-    public static function getDataCurrent($request) {
+    public function refKolektibilitas()
+    {
+        // sementara diarahkan ke nik
+        return $this->belongsTo(Kolektibilitas::class,'nik','nik');
 
     }
 }
