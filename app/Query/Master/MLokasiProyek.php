79d5ae78c1802fe421da7b9bf9f28cabbd7c14ea
rexy siwabessy <rexysiwabessy@gmail.com>
2022-07-22 00:05:27 +0700

    master

---
+++ app/Query/Master/MLokasiProyek.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Query\Master;
+
+use App\Models\Master\MLokasiProyek as Model;
+use App\Constants\Constants;
+
+class MLokasiProyek
+{
+    public static function byId($id)
+    {
+        return ['items' => Model::find($id)];
+    }
+
+    public static function getAll($request)
+    {
+        try {
+            if($request->dropdown == Constants::IS_ACTIVE) $request->limit = Model::count();
+            $data = Model::where(function ($query) use ($request){
+                if($request->nama) $query->where('nama','ilike',"%$request->nama%");
+            })->paginate($request->limit);
+            return [
+                'items' => $data->getCollection()->transform(function ($item){
+                    return $item;
+                }),
+                'attributes' => [
+                    'total' => $data->total(),
+                    'current_page' => $data->currentPage(),
+                    'from' => $data->currentPage(),
+                    'per_page' => (int) $data->perPage(),
+                ]
+            ];
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public static function store($request){
+
+    }
+
+    public static function updated($request){
+
+    }
+
+    public static function destroy($request){
+
+    }
+}
