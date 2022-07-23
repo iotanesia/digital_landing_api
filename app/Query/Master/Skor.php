23f2da5cea6826729d46e321994539da0e473d81
Gilang Dwi <gilangdwi13@gmail.com>
2022-07-23 12:04:47 +0700

    skoring

--- app/Query/Master/Skor.php
+++ app/Query/Master/Skor.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App\Models\Master;
+namespace App\Query\Master;
 
 use App\Models\Master\Skor as Model;
 
 class Skor
 {
-
+    public static function byId($id)
+    {
+        return Model::find($id);
+    }
 }
